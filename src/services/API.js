42387ff656bbbbe0296396c1b9e09a6452170ef7
Folayan Adesola <folayanshola@gmail.com>
2026-06-17 19:23:52 +0100

    gh

--- src/services/API.js
+++ src/services/API.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 // Create Axios instance
-const base_url = import.meta.env.VITE_API_URL
-//const base_url = "https://fms-api.customs.gov.ng/api"
+//const base_url = import.meta.env.VITE_API_URL
+const base_url = "https://fms-api.customs.gov.ng/api"
 
 export const Api = axios.create({
   baseURL: base_url,
